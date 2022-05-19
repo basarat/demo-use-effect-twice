823c632924ca1e4e31008425665cde6ae96f5330
basarat <basaratali@gmail.com>
2022-05-20 09:38:05 +1000

    wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { Timer } from './Timer';
+import { useEffect, useState } from 'react';
+
+export const CustomTimer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setSeconds(seconds => seconds + 1),
+      1000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <h1>Custom: {seconds}</h1>
+  )
+};
+
 function App() {
   return (
-    null
+    <>
+      <Timer />
+      <CustomTimer />
+    </>
   );
 }
 
---
+++ src/Timer.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+export const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setSeconds(seconds => seconds + 1),
+      1000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <h1>Seconds: {seconds}</h1>
+  )
+};
